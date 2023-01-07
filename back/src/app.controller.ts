b14b54a26666f421fd30cf49d24d8224ba2a7a59
k.popkov@bk.ru <8821022qweasd>
2023-01-08 00:48:29 +0300

    update

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from "./services/prisma.services";
+import { hash } from 'bcrypt';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly prismaService: PrismaService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post('/login')
+    async login(@Body() body) {
+        return body
+
+    }
+
+    @Post('/registration')
+    async registration(@Body() body) {
+        const hashedPassword = await hash(body.password, 10)
+        const result = await this.prismaService.users.create({
+            data: {
+                ...body,
+                password: hashedPassword
+            }
+        })
+        console.log(result)
+        return {
+            status: 'ok',
+            message: `Пользователь ${body.email} зарегистрированн`
+        }
+    }
 }
