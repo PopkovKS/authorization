60403365b64d10aefdcd64ce3f19ab2b2e2e6293
k.popkov@bk.ru <8821022qweasd>
2023-01-08 22:47:44 +0300

    update

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from "./services/prisma.services";
 import { hash } from 'bcrypt';
+import { Prisma } from "@prisma/client";
 
 @Controller()
 export class AppController {
@@ -24,17 +25,31 @@ export class AppController {
 
     @Post('/registration')
     async registration(@Body() body) {
-        const hashedPassword = await hash(body.password, 10)
-        const result = await this.prismaService.users.create({
-            data: {
-                ...body,
-                password: hashedPassword
+        try {
+            const hashedPassword = await hash(body.password, 10)
+
+            const result = await this.prismaService.users.create({
+                data: {
+                    ...body,
+                    password: hashedPassword
+                }
+
+            })
+            console.log(result)
+            return {
+                status: 'ok',
+                message: `Пользователь ${body.email} зарегистрированн`
             }
-        })
-        console.log(result)
-        return {
-            status: 'ok',
-            message: `Пользователь ${body.email} зарегистрированн`
+
+        } catch (e) {
+            if (
+                e instanceof Prisma.PrismaClientKnownRequestError &&
+                e.code === 'P2002'
+            ) {
+                throw new ConflictException(`Email ${body.email} already used.`);
+            } else {
+                throw new Error(e);
+            }
         }
     }
 }
--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from "../local.strategy";
 import { PassportModule } from "@nestjs/passport";
+import { PrismaService } from "../services/prisma.services";
 
 @Module({
-  imports: [PassportModule],
-  providers: [AuthService,,
-    LocalStrategy,]
+  imports: [ PassportModule],
+  providers: [
+      AuthService,
+    LocalStrategy,
+    PrismaService
+  ]
 })
 export class AuthModule {}
