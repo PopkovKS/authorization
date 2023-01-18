c2c5b69002ef2397bb9ae877f141c16edda647d2
k.popkov@bk.ru <8821022qweasd>
2023-01-19 02:57:54 +0300

    update

--- front/src/environments/environments.prod.ts
+++ front/src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 import { environment as env } from './environment';
 
-export const environment = {
+export const environment:any = {
   ...env,
   production: true,
   BASE_URL_API: "http://popkovks.ru/api"
