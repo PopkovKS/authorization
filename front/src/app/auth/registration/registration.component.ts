c1ff70b7f14b6fd5810fa5292d233955a418df76
k.popkov@bk.ru <8821022qweasd>
2023-01-19 19:26:25 +0300

    update

--- front/src/app/auth/registration/registration.component.ts
+++ front/src/app/auth/registration/registration.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from "@angular/common/http";
 import { HttpService } from "../../http.service";
 
 @Component({
--- front/src/app/http.service.ts
+++ front/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { catchError, Observable, of, throwError } from "rxjs";
 import { NzNotificationService } from "ng-zorro-antd/notification";
 import { environment } from '../environments/environment';
