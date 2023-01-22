462cc7a0371280e6c36b416a41c3940120154606
k.popkov@bk.ru <k.popkov@bk.ru>
2023-01-22 19:37:51 +0300

    add item

---
+++ front/src/app/pages/items/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent {
+
+
+  @Input() item: any
+  @Output() onClose = new EventEmitter<any>()
+  Close() {
+    this.onClose.emit()
+  }
+}
