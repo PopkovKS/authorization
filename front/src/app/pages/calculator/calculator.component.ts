90d74219cd5e3ae236499811c0ac4bc3c582a615
k.popkov@bk.ru <k.popkov@bk.ru>
2023-01-25 01:01:18 +0300

    add calc

---
+++ front/src/app/pages/calculator/calculator.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.scss']
+})
+export class CalculatorComponent {
+
+  start: any;
+  value: any;
+
+  sum() {
+   this.start
+  }
+
+}
